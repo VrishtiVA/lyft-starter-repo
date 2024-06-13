ce61273174001b6bf12e8264297d247886043a67
VrishtiA <va104@student.le.ac.uk>
2024-06-13 21:34:33 +0100

    Running all tests

---
+++ test/run_tests.py
@@ -0,0 +1,15 @@
+
+import unittest
+
+from test.test_engine.test_capulet_engine import TestCapuletEngine
+from test.test_engine.test_sternman_engine import TestSternmanEngine
+from test.test_engine.test_willoughby_engine import TestWilloughbyEngine
+
+from test.test_battery.test_spindler_battery import TestSpindlerBattery
+from test.test_battery.test_nubbin_battery import TestNubbinBattery
+
+from test.test_tires.test_carrigan_tire import TestCarriganTire
+from test.test_tires.test_octoprime_tire import TestOctoprimeTire
+
+if __name__ == '__main__':
+    unittest.main()
