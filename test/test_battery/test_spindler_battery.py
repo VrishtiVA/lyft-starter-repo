9110a1ff6d80e16c176610dc5e64f214a8f40650
VrishtiA <va104@student.le.ac.uk>
2024-06-13 18:43:25 +0100

    Reflecting on feedback and making some improvements

---
+++ test/test_battery/test_spindler_battery.py
@@ -0,0 +1,23 @@
+
+import unittest
+from datetime import datetime
+
+from batteries.spindler_battery import SpindlerBattery
+
+class TestSpindlerBattery(unittest.TestCase):
+
+    def test_battery_should_be_serviced(self):
+
+        today = datetime.today().date()
+        last_service_date = today.replace(year=today.year - 3)
+
+        battery = SpindlerBattery(last_service_date, today)
+        self.assertTrue(battery.needs_service())
+
+    def test_battery_should_not_be_serviced(self):
+
+        today = datetime.today().date()
+        last_service_date = today.replace(year=today.year - 1)
+
+        battery = SpindlerBattery(last_service_date, today)
+        self.assertFalse(battery.needs_service())
---
+++ test/test_engine/test_sternman_engine.py
@@ -0,0 +1,20 @@
+
+import unittest
+
+from engines.sternman_engine import SternmanEngine
+
+class TestSternmanEngine(unittest.TestCase):
+        
+    def test_engine_should_be_serviced(self):
+
+        warning_light_on = True
+
+        engine = SternmanEngine(warning_light_on)
+        self.assertTrue(engine.needs_service())
+
+    def test_engine_should_not_be_serviced(self):
+
+        warning_light_on = False
+
+        engine = SternmanEngine(warning_light_on)
+        self.assertFalse(engine.needs_service())
